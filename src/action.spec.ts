6dba47982886d9243f7262dd4f64f7dd0652b5f9
Alex Miller <codex.nz@gmail.com>
2022-04-12 16:57:20 +1200

    feat: Implement action definition

---
+++ src/action.spec.ts
@@ -0,0 +1,65 @@
+import * as core from "@actions/core";
+import { ActionConfig, getConfig } from "./action";
+
+describe("Action", () => {
+  describe("getConfig", () => {
+    // Represent the process.env inputs.
+    let mockEnvConfig: any;
+
+    beforeEach(() => {
+      mockEnvConfig = {
+        token: "secret",
+        workflowName: "test workflow",
+        checkName: "test check",
+        timeout_mins: "15",
+        poll_interval_ms: "5000",
+      };
+
+      jest.spyOn(core, "getInput").mockImplementation((input: string) => {
+        switch (input) {
+          case "token":
+            return mockEnvConfig.token;
+          case "workflow_name":
+            return mockEnvConfig.workflowName;
+          case "check_name":
+            return mockEnvConfig.checkName;
+          case "timeout_mins":
+            return mockEnvConfig.timeout_mins;
+          case "poll_interval_ms":
+            return mockEnvConfig.poll_interval_ms;
+          default:
+            throw new Error("invalid input requested");
+        }
+      });
+    });
+
+    afterEach(() => {
+      jest.restoreAllMocks();
+    });
+
+    it("should return a valid config", () => {
+      const config: ActionConfig = getConfig();
+
+      // Assert that the numbers / types have been properly loaded.
+      expect(config.token).toStrictEqual("secret");
+      expect(config.workflowName).toStrictEqual("test workflow");
+      expect(config.checkName).toStrictEqual("test check");
+      expect(config.timeoutMins).toStrictEqual(15);
+      expect(config.pollIntervalMs).toStrictEqual(5000);
+    });
+
+    it("should provide a default run timeout if none is supplied", () => {
+      mockEnvConfig.run_timeout_seconds = "";
+      const config: ActionConfig = getConfig();
+
+      expect(config.timeoutMins).toStrictEqual(15);
+    });
+
+    it("should provide a default polling interval if none is supplied", () => {
+      mockEnvConfig.poll_interval_ms = "";
+      const config: ActionConfig = getConfig();
+
+      expect(config.pollIntervalMs).toStrictEqual(5000);
+    });
+  });
+});
---
+++ src/action.ts
@@ -0,0 +1,70 @@
+import * as core from "@actions/core";
+
+const WORKFLOW_TIMEOUT_MINUTES = 15;
+const POLL_INTERVAL_MS = 5000;
+
+/**
+ * action.yaml definition.
+ */
+export interface ActionConfig {
+  /**
+   * GitHub API token for making requests.
+   */
+  token: string;
+
+  /**
+   * The workflow that you wish to await completion of.
+   */
+  workflowName: string;
+
+  /**
+   * A specific check within the workflow to wait for. Await all checks if this is not specified.
+   */
+  checkName?: string;
+
+  /**
+   * Time until giving up on the completion of an action.
+   * @default 15
+   */
+  timeoutMins: number;
+
+  /**
+   * Frequency to poll the action for a status.
+   * @default 5000
+   */
+  pollIntervalMs: number;
+}
+
+export function getConfig(): ActionConfig {
+  return {
+    token: core.getInput("token", { required: true }),
+    workflowName: core.getInput("workflow_name", { required: true }),
+    checkName: (() => {
+      const input = core.getInput("check_name");
+      return input === "" ? undefined : input;
+    })(),
+    timeoutMins:
+      getNumberFromValue(core.getInput("timeout_mins")) ||
+      WORKFLOW_TIMEOUT_MINUTES,
+    pollIntervalMs:
+      getNumberFromValue(core.getInput("poll_interval_ms")) || POLL_INTERVAL_MS,
+  };
+}
+
+function getNumberFromValue(value: string): number | undefined {
+  if (value === "") {
+    return undefined;
+  }
+
+  try {
+    const num = parseInt(value);
+
+    if (isNaN(num)) {
+      throw new Error("Parsed value is NaN");
+    }
+
+    return num;
+  } catch {
+    throw new Error(`Unable to parse value: ${value}`);
+  }
+}
